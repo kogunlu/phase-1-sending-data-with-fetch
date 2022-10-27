80cbc085f2a47bd105af8dc38bc350fdbf8b66bc
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-10-27 23:23:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(userName, userEmail){
+
+return fetch('http://localhost:3000/users', {
+   method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        "Accept": 'application/json'
+    },
+    body: JSON.stringify({
+        name: userName,
+        email: userEmail
+    })
+}).then(response => response.json())
+.then(data => {
+document.querySelector("body").append(data.id)
+})
+.catch(err => {
+    // alert('Something went wrong!')
+    // const p = document.createElement('p')
+    // p.innerText = err.message
+    // document.querySelector("body").append(p)
+    // console.log(err.message)
+document.querySelector("body").innerHTML = err.message
+console.log(document.querySelector("body").innerHTML)
+}
+)
+
+}
